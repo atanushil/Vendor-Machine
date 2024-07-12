fd4e336228bbfc2e021eaeb0c9ddb1e1460a7b99
Atanu Shil <atanushil358@gmail.com>
2024-07-11 20:38:01 -0700

    update the log in page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-// import NavBar from "./components/NavBar";
-
+import { useState } from "react";
 import Login from "./components/Login";
+import NavBar from "./components/NavBar";
 
-
+const menuItems = [
+  { label: "Home", link: "/" },
+  { label: "Upload Segment", link: "/upload" },
+  { label: "Download Segment", link: "/download" },
+];
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Initialize to false initially
+
   return (
     <div>
-      <Login/>
+      <div className="min-h-screen flex flex-col">
+        <NavBar
+          isLoggedIn={isLoggedIn}
+          setIsLoggedIn={setIsLoggedIn}
+          menuItems={menuItems}
+        />
+        <main className="flex-grow flex items-center justify-center bg-gray-100 p-4">
+          {!isLoggedIn && <Login setIsLoggedIn={setIsLoggedIn} />}
+        </main>
+      </div>
     </div>
   );
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,64 +1,48 @@
-import React, { useState } from "react";
-import NavBar from "./NavBar";
+import React from "react";
 
-const menuItems = [
-  { label: "Home", link: "/" },
-  { label: "Upload Segment", link: "/upload" },
-  { label: "Download Segment", link: "/download" },
-];
-
-export default function Login() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true); // Initialize to false initially
-
+export default function Login({ setIsLoggedIn }) {
   return (
-    <div className="min-h-screen flex flex-col">
-      <NavBar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} menuItems={menuItems} />
-      <main className="flex-grow flex items-center justify-center bg-gray-100 p-4">
-        {!isLoggedIn && (
-          <div className="max-w-md w-full bg-white p-8 shadow-md rounded-lg">
-            <h1 className="text-3xl font-bold text-center mb-6">
-              Vendor Machine Login
-            </h1>
-            <form
-              onSubmit={(e) => {
-                e.preventDefault();
-                setIsLoggedIn(true);
-              }}
-            >
-              <div className="mb-4">
-                <label htmlFor="email" className="block text-gray-700">
-                  User Email Id
-                </label>
-                <input
-                  required
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
-                />
-              </div>
-              <div className="mb-6">
-                <label htmlFor="password" className="block text-gray-700">
-                  Password
-                </label>
-                <input
-                  required
-                  type="password"
-                  id="password"
-                  name="password"
-                  className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
-                />
-              </div>
-              <button
-                type="submit"
-                className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600"
-              >
-                Log In
-              </button>
-            </form>
-          </div>
-        )}
-      </main>
+    <div className="max-w-md w-full bg-white p-8 shadow-md rounded-lg">
+      <h1 className="text-3xl font-bold text-center mb-6">
+        Vendor Machine Login
+      </h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setIsLoggedIn(true);
+        }}
+      >
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-gray-700">
+            User Email Id
+          </label>
+          <input
+            required
+            type="email"
+            id="email"
+            name="email"
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+          />
+        </div>
+        <div className="mb-6">
+          <label htmlFor="password" className="block text-gray-700">
+            Password
+          </label>
+          <input
+            required
+            type="password"
+            id="password"
+            name="password"
+            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600"
+        >
+          Log In
+        </button>
+      </form>
     </div>
   );
 }
