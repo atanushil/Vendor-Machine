bceb5d8e631063028780f6bc6fb7184c2cf17bfb
Atanu Shil <atanushil358@gmail.com>
2024-07-12 20:41:07 -0700

    updated dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 export default function Dashboard() {
   return (
-    <div>
-      from dashboard
+    <div className="max-w-3xl mx-auto bg-white p-8 shadow-md rounded-lg">
+      <h1 className="text-3xl font-bold text-center mb-4">Welcome to the Vendor Machine</h1>
+      <p className="text-lg text-center">Choose an option from the menu to get started!</p>
     </div>
-  )
+  );
 }
